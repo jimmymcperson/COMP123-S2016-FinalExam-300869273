a6389f039e0db300fd9179935348e6cb63909e68
jimmymcperson <undeadwalrus@hotmail.com>
2016-08-19 10:53:26 -0400

    Initial commit.

---
+++ COMP123-S2016-FinalExam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * ==================================================================================================
+ * App Name: Character Creation
+ * Author's Name: Jonathan Hao
+ * Student#: 300869273
+ * Creation Date: August 19, 2016
+ * Description: This program creates a fantasy character with traits.
+ * Version: 0.0.1 - Initial commit.
+ * ==================================================================================================
+ */
+
+namespace COMP123_S2016_FinalExam
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+        public static SplashForm splashForm = new SplashForm();
+        public static GenerateNameForm generateNameForm = new GenerateNameForm();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(splashForm);
+        }
+    }
+}
---
+++ COMP123-S2016-FinalExam/SplashForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * ==================================================================================================
+ * App Name: Character Creation
+ * Author's Name: Jonathan Hao
+ * Student#: 300869273
+ * Creation Date: August 19, 2016
+ * Description: This program creates a fantasy character with traits.
+ * Version: 0.0.1 - Initial commit.
+ * ==================================================================================================
+ */
+
+namespace COMP123_S2016_FinalExam
+    {
+
+    /** <summary>
+    * This class defines a form for a splash screen.
+    * </summary>
+    * 
+    * @class SplashForm
+    * @extends Form
+    */
+    public partial class SplashForm : Form
+        {
+
+        /** <summary>
+        * This is the default constructor.
+        * </summary>
+        *
+        * @method SplashForm
+        */
+        public SplashForm()
+            {
+            InitializeComponent();
+            }
+
+        /** <summary>
+        * This method closes the form after 3 seconds.
+        * </summary>
+        *
+        * @method SplashFormTimer_Tick
+        * @params {object} sender
+        * @params {EventArgs} e
+        * @private
+        * @returns {void}
+        */
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+            {
+            SplashFormTimer.Enabled = false;
+            this.Hide();
+            Program.generateNameForm.Show();
+            }
+        }
+    }
